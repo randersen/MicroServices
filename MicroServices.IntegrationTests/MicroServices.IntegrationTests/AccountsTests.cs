5991d368a2eb516df62c6e0518fa897affbd3e4c
ryan.andersen91 <ryan.andersen91@gmail.com>
2017-07-25 22:55:54 -0700

    Added test solution

---
+++ MicroServices.IntegrationTests/MicroServices.IntegrationTests/AccountsTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicroServices.Client;
+using MicroServices.Models.Accounts;
+using NUnit.Framework;
+using RestSharp;
+
+namespace MicroServices.IntegrationTests
+{
+    [TestFixture]
+    public class AccountsTests
+    {
+        [Category("Payments")]
+        [Category("Accounts")]
+        [Test]
+        public async Task GetAccountByAccountIdReturnsAccount()
+        {
+            var account = await AccountsHelpers.GetAccountById(1).ConfigureAwait(false);
+
+            Assert.AreEqual(1, account.Id);
+            Assert.AreEqual("Fred McFeddington", account.Name);
+            Assert.AreEqual(1, account.BankAccounts.Count);
+
+            var bankAccount = account.BankAccounts.First();
+            Assert.AreEqual("12345678", bankAccount.AccountNumber);
+            Assert.AreEqual("123", bankAccount.RoutingNumber);
+            Assert.AreEqual(1, bankAccount.Id);
+        }
+    }
+
+    public class AccountsHelpers
+    {
+
+        private static readonly RestClient _client = ContractClientProvider.Client().Accounts();
+
+        public static async Task<Account> GetAccountById(int accountId)
+        {
+            var request = new RestRequest($"accounts?id={accountId}", Method.GET);
+            var response = await _client.ExecuteGetTaskAsync<Account>(request).ConfigureAwait(false);
+            return response.Data;
+        }
+    }
+}
---
+++ MicroServices.IntegrationTests/MicroServices.IntegrationTests/PaymentsTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicroServices.Client;
+using MicroServices.Models.Payments;
+using NUnit.Framework;
+using RestSharp;
+
+namespace MicroServices.IntegrationTests
+{
+    [TestFixture]
+    public class PaymentsTests
+    {
+        [Test]
+        [Category("Payments")]
+        public async Task GetBankAccountByAccountIdReturnsBankAccounts()
+        {
+            var bankAccounts = await PaymentsHelpers.GetBankAccountsForAccount(1).ConfigureAwait(false);
+            Assert.AreEqual(1, bankAccounts.Count);
+            var bankAccount = bankAccounts.First();
+
+            Assert.AreEqual("12345678", bankAccount.AccountNumber);
+            Assert.AreEqual("123", bankAccount.RoutingNumber);
+            Assert.AreEqual(1, bankAccount.Id);
+        }
+
+    }
+
+    public class PaymentsHelpers
+    {
+        private static readonly RestClient _client = ContractClientProvider.Client().Payments();
+
+        public static async Task<List<BankAccount>> GetBankAccountsForAccount(int accountId)
+        {
+            var request = new RestRequest($"bank-accounts?id={accountId}", Method.GET);
+            var response = await _client.ExecuteGetTaskAsync<List<BankAccount>>(request).ConfigureAwait(false);
+            return response.Data;
+        }
+    }
+}
